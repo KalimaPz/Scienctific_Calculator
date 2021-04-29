7fafe38eec62f21d0c03daff7de19e4ff2a1eecd
Donnukrit <kalima007c@gmail.com>
2021-04-30 02:30:08 +0700

    performed action

--- main.java
+++ main.java
@@ -1,25 +1,479 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.util.ArrayList;
 
 class CalculatorGUI extends JFrame {
-    JTextField textField;
+    JTextField inputTextField = new JTextField("");
+    JButton b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, bPeriod, bPi, bDel, bClr, bMul, bDiv, bAdd, bSubt, bE, bEqual,bSin,bCos,bTan,bLog,bLn,bSq,bPow,bEPow,bInv,bSqrt ;
+    JPanel buttonPanel = new JPanel();
+    void buildFunctionalPanel() {
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+       
+        gbc.anchor = GridBagConstraints.PAGE_START;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        gbc.insets = new Insets(2, 2, 2, 2);
+        b0 = new JButton("0");
+        b1 = new JButton("1");
+        b2 = new JButton("2");
+        b3 = new JButton("3");
+        b4 = new JButton("4");
+        b5 = new JButton("5");
+        b6 = new JButton("6");
+        b7 = new JButton("7");
+        b8 = new JButton("8");
+        b9 = new JButton("9");
+        
+        bSin = new JButton("Sin");
+        bCos = new JButton("Cos");
+        bTan = new JButton("Tan");
+        
+        bLog = new JButton("Log");
+        bLn = new JButton("ln");
+        bSq = new JButton("x^2");
+        bPow = new JButton("x^y");
+        bEPow = new JButton("e^x");
+
+        bInv = new JButton("1/x");
+
+        bSqrt = new JButton("Sqrt");
+        
+        bClr = new JButton("CLR");
+        bDel = new JButton("DEL");
+
+        
+        bAdd   = new JButton("+");
+        bSubt = new JButton("-");
+        bMul = new JButton("*");
+        bDiv = new JButton("/");
+        
+        bPi = new JButton("PI");
+        bPeriod = new JButton(".");
+        bE = new JButton("e");
+        bEqual = new JButton("=");
+        b0 = new JButton("0");
+        b1 = new JButton("1");
+        b2 = new JButton("2");
+        b3 = new JButton("3");
+        b4 = new JButton("4");
+        b5 = new JButton("5");
+        b6 = new JButton("6");
+        b7 = new JButton("7");
+        b8 = new JButton("8");
+        b9 = new JButton("9");
+        
+        bSin = new JButton("Sin");
+        bCos = new JButton("Cos");
+        bTan = new JButton("Tan");
+        
+        bLog = new JButton("Log");
+        bLn = new JButton("ln");
+        bSq = new JButton("x^2");
+        bPow = new JButton("x^y");
+        bEPow = new JButton("e^x");
+
+        bInv = new JButton("1/x");
+
+        bSqrt = new JButton("Sqrt");
+        
+        bClr = new JButton("CLR");
+        bDel = new JButton("DEL");
+
+        
+        bAdd = new JButton("+");
+        bSubt = new JButton("-");
+        bMul = new JButton("*");
+        bDiv = new JButton("/");
+        
+        bPi = new JButton("PI");
+        bPeriod = new JButton(".");
+        bE = new JButton("e");
+        bEqual = new JButton("=");
+        
+        buttonPanel.add(bCos);
+        buttonPanel.add(bSin);
+        buttonPanel.add(bTan);
+        buttonPanel.add(bLog);
+        buttonPanel.add(bLn);
+
+        buttonPanel.add(bSq);
+        buttonPanel.add(bPow);
+        buttonPanel.add(bEPow);
+        buttonPanel.add(bInv);
+        buttonPanel.add(bSqrt);
+
+        buttonPanel.add(b7);
+        buttonPanel.add(b8);
+        buttonPanel.add(b9);
+        buttonPanel.add(bDel);
+        buttonPanel.add(bClr);
+    
+        buttonPanel.add(b4);
+        buttonPanel.add(b5);
+        buttonPanel.add(b6);
+        buttonPanel.add(bMul);
+        buttonPanel.add(bDiv);
+
+        buttonPanel.add(b1);
+        buttonPanel.add(b2);
+        buttonPanel.add(b3);
+        buttonPanel.add(bAdd);
+        buttonPanel.add(bSubt);
+
+        buttonPanel.add(b0);
+        buttonPanel.add(bPeriod);
+        buttonPanel.add(bPi);
+        buttonPanel.add(bE);
+        buttonPanel.add(bEqual);
+
+
+
+
+
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+		String s = e.getActionCommand();
+		if (s.equals("1")) {
+			if (z == 0) {
+				tfield.setText(tfield.getText() + "1");
+			} else {
+				tfield.setText("");
+				tfield.setText(tfield.getText() + "1");
+				z = 0;
+			}
+		}
+		if (s.equals("2")) {
+			if (z == 0) {
+				tfield.setText(tfield.getText() + "2");
+			} else {
+				tfield.setText("");
+				tfield.setText(tfield.getText() + "2");
+				z = 0;
+			}
+		}
+		if (s.equals("3")) {
+			if (z == 0) {
+				tfield.setText(tfield.getText() + "3");
+			} else {
+				tfield.setText("");
+				tfield.setText(tfield.getText() + "3");
+				z = 0;
+			}
+		}
+		if (s.equals("4")) {
+			if (z == 0) {
+				tfield.setText(tfield.getText() + "4");
+			} else {
+				tfield.setText("");
+				tfield.setText(tfield.getText() + "4");
+				z = 0;
+			}
+		}
+		if (s.equals("5")) {
+			if (z == 0) {
+				tfield.setText(tfield.getText() + "5");
+			} else {
+				tfield.setText("");
+				tfield.setText(tfield.getText() + "5");
+				z = 0;
+			}
+		}
+		if (s.equals("6")) {
+			if (z == 0) {
+				tfield.setText(tfield.getText() + "6");
+			} else {
+				tfield.setText("");
+				tfield.setText(tfield.getText() + "6");
+				z = 0;
+			}
+		}
+		if (s.equals("7")) {
+			if (z == 0) {
+				tfield.setText(tfield.getText() + "7");
+			} else {
+				tfield.setText("");
+				tfield.setText(tfield.getText() + "7");
+				z = 0;
+			}
+		}
+		if (s.equals("8")) {
+			if (z == 0) {
+				tfield.setText(tfield.getText() + "8");
+			} else {
+				tfield.setText("");
+				tfield.setText(tfield.getText() + "8");
+				z = 0;
+			}
+		}
+		if (s.equals("9")) {
+			if (z == 0) {
+				tfield.setText(tfield.getText() + "9");
+			} else {
+				tfield.setText("");
+				tfield.setText(tfield.getText() + "9");
+				z = 0;
+			}
+		}
+		if (s.equals("0")) {
+			if (z == 0) {
+				tfield.setText(tfield.getText() + "0");
+			} else {
+				tfield.setText("");
+				tfield.setText(tfield.getText() + "0");
+				z = 0;
+			}
+		}
+		if (s.equals("AC")) {
+			tfield.setText("");
+			// x = 0;
+			// y = 0;
+			// z = 0;
+		}
+		if (s.equals("log")) {
+			if (tfield.getText().equals("")) {
+				tfield.setText("");
+			} else {
+				a = Math.log(Double.parseDouble(tfield.getText()));
+				tfield.setText("");
+				tfield.setText(tfield.getText() + a);
+			}
+		}
+		if (s.equals("1/x")) {
+			if (tfield.getText().equals("")) {
+				tfield.setText("");
+			} else {
+				a = 1 / Double.parseDouble(tfield.getText());
+				tfield.setText("");
+				tfield.setText(tfield.getText() + a);
+			}
+		}
+		if (s.equals("Exp")) {
+			if (tfield.getText().equals("")) {
+				tfield.setText("");
+			} else {
+				a = Math.exp(Double.parseDouble(tfield.getText()));
+				tfield.setText("");
+				tfield.setText(tfield.getText() + a);
+			}
+		}
+		if (s.equals("x^2")) {
+			if (tfield.getText().equals("")) {
+				tfield.setText("");
+			} else {
+				a = Math.pow(Double.parseDouble(tfield.getText()), 2);
+				tfield.setText("");
+				tfield.setText(tfield.getText() + a);
+			}
+		}
+		if (s.equals("x^3")) {
+			if (tfield.getText().equals("")) {
+				tfield.setText("");
+			} else {
+				a = Math.pow(Double.parseDouble(tfield.getText()), 3);
+				tfield.setText("");
+				tfield.setText(tfield.getText() + a);
+			}
+		}
+		if (s.equals("+/-")) {
+			if (x == 0) {
+				tfield.setText("-" + tfield.getText());
+				x = 1;
+			} else {
+				tfield.setText(tfield.getText());
+			}
+		}
+		if (s.equals(".")) {
+			if (y == 0) {
+				tfield.setText(tfield.getText() + ".");
+				y = 1;
+			} else {
+				tfield.setText(tfield.getText());
+			}
+		}
+		if (s.equals("+")) {
+			if (tfield.getText().equals("")) {
+				tfield.setText("");
+				temp = 0;
+				ch = '+';
+			} else {
+				temp = Double.parseDouble(tfield.getText());
+				tfield.setText("");
+				ch = '+';
+				y = 0;
+				x = 0;
+			}
+			tfield.requestFocus();
+		}
+		if (s.equals("-")) {
+			if (tfield.getText().equals("")) {
+				tfield.setText("");
+				temp = 0;
+				ch = '-';
+			} else {
+				x = 0;
+				y = 0;
+				temp = Double.parseDouble(tfield.getText());
+				tfield.setText("");
+				ch = '-';
+			}
+			tfield.requestFocus();
+		}
+		if (s.equals("/")) {
+			if (tfield.getText().equals("")) {
+				tfield.setText("");
+				temp = 1;
+				ch = '/';
+			} else {
+				x = 0;
+				y = 0;
+				temp = Double.parseDouble(tfield.getText());
+				ch = '/';
+				tfield.setText("");
+			}
+			tfield.requestFocus();
+		}
+		if (s.equals("*")) {
+			if (tfield.getText().equals("")) {
+				tfield.setText("");
+				temp = 1;
+				ch = '*';
+			} else {
+				x = 0;
+				y = 0;
+				temp = Double.parseDouble(tfield.getText());
+				ch = '*';
+				tfield.setText("");
+			}
+			tfield.requestFocus();
+		}
+		if (s.equals("MC")) {
+			m1 = 0;
+			tfield.setText("");
+		}
+		if (s.equals("MR")) {
+			tfield.setText("");
+			tfield.setText(tfield.getText() + m1);
+		}
+		if (s.equals("M+")) {
+			if (k == 1) {
+				m1 = Double.parseDouble(tfield.getText());
+				k++;
+			} else {
+				m1 += Double.parseDouble(tfield.getText());
+				tfield.setText("" + m1);
+			}
+		}
+		if (s.equals("M-")) {
+			if (k == 1) {
+				m1 = Double.parseDouble(tfield.getText());
+				k++;
+			} else {
+				m1 -= Double.parseDouble(tfield.getText());
+				tfield.setText("" + m1);
+			}
+		}
+		if (s.equals("Sqrt")) {
+			if (tfield.getText().equals("")) {
+				tfield.setText("");
+			} else {
+				a = Math.sqrt(Double.parseDouble(tfield.getText()));
+				tfield.setText("");
+				tfield.setText(tfield.getText() + a);
+			}
+		}
+		if (s.equals("SIN")) {
+			if (tfield.getText().equals("")) {
+				tfield.setText("");
+			} else {
+				a = Math.sin(Double.parseDouble(tfield.getText()));
+				tfield.setText("");
+				tfield.setText(tfield.getText() + a);
+			}
+		}
+		if (s.equals("COS")) {
+			if (tfield.getText().equals("")) {
+				tfield.setText("");
+			} else {
+				a = Math.cos(Double.parseDouble(tfield.getText()));
+				tfield.setText("");
+				tfield.setText(tfield.getText() + a);
+			}
+		}
+		if (s.equals("TAN")) {
+			if (tfield.getText().equals("")) {
+				tfield.setText("");
+			} else {
+				a = Math.tan(Double.parseDouble(tfield.getText()));
+				tfield.setText("");
+				tfield.setText(tfield.getText() + a);
+			}
+		}
+		if (s.equals("=")) {
+			if (tfield.getText().equals("")) {
+				tfield.setText("");
+			} else {
+				temp1 = Double.parseDouble(tfield.getText());
+				switch (ch) {
+				case '+':
+					result = temp + temp1;
+					break;
+				case '-':
+					result = temp - temp1;
+					break;
+				case '/':
+					result = temp / temp1;
+					break;
+				case '*':
+					result = temp * temp1;
+					break;
+				}
+				tfield.setText("");
+				tfield.setText(tfield.getText() + result);
+				z = 1;
+			}
+		}
+		if (s.equals("n!")) {
+			if (tfield.getText().equals("")) {
+				tfield.setText("");
+			} else {
+				a = fact(Double.parseDouble(tfield.getText()));
+				tfield.setText("");
+				tfield.setText(tfield.getText() + a);
+			}
+		}
+		tfield.requestFocus();
+	}
+
+	double fact(double x) {
+		int er = 0;
+		if (x < 0) {
+			er = 20;
+			return 0;
+		}
+		double i, s = 1;
+		for (i = 2; i <= x; i += 1.0)
+			s *= i;
+		return s;
+	}
     CalculatorGUI() {
         super("Calculator");
         setLayout(new BorderLayout());
-     
-    
+        add(inputTextField,BorderLayout.NORTH);
+        buildFunctionalPanel();
+        add(buttonPanel,BorderLayout.CENTER);
     }
-    
 }
+
 class Main {
     public static void main(String[] args) {
         CalculatorGUI frame = new CalculatorGUI();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 800);
-        frame.setResizable(false);
-
         frame.setVisible(true);
+        frame.setSize(700, 600);
+        // frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 }
